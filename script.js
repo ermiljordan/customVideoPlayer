21f3ac7d56640bcac2b4d7b65d7fee6cc954513f
chiwwy <ermiljordan64@gmail.com>
2018-02-13 21:38:02 +0900

    add function

---
+++ script.js
@@ -0,0 +1,13 @@
+// Get our elemets
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle')
+const skipButton = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+// Build our functions 
+
+// Hoop up he event listener 
